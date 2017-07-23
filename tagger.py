27827be7c869409ee5a4eba56ce72809799b96f1
jjmachan <jamesjithin97@gmail.com>
2017-07-23 15:08:58 +0530

    tagger complete

--- tagger.py
+++ tagger.py
@@ -9,6 +9,7 @@ from bs4 import BeautifulSoup
 
 
 URL = "http://www.iiitmk.ac.in/MalayalamPOSTagger/"
+CORPUS = 'corpus/'
 driver = webdriver.Chrome("/usr/bin/chromedriver_linux64/chromedriver")
 driver.get(URL)
 assert "Malayalam" in driver.title
@@ -17,23 +18,25 @@ dirList = [x[0] for x in os.walk("./Data")]
 dirList = dirList[1:]
 
 for domains in dirList:
-	for files in os.listdir(domains):
-		file = codecs.open(domains+"/"+files,encoding='utf-8')
-		print("printing file: "+domains+"/"+files)
-		textArea = driver.find_element_by_id("mal_text")
-		textArea.clear()
-		for (i,line) in enumerate(file):
-			if i < 8:
-				continue
-			textArea.send_keys(line)
-		button = driver.find_element_by_xpath("//input[@value='Tag The Sentence']")
-		button.click()
-		page_source = driver.page_source
-		soup = BeautifulSoup(page_source, 'html.parser') 
-		for line in soup.find_all(attrs ={"size":"3"}):
-			print(line.string)
-		break
+	if not os.path.exists(CORPUS):
+		os.makedirs(CORPUS)
+	with open(CORPUS+domains.split('/')[2], "wb") as f:
+		s = ''
+		for files in os.listdir(domains):
+			file = codecs.open(domains+"/"+files,encoding='utf-8')
+			print("printing file: "+domains+"/"+files)
+			textArea = driver.find_element_by_id("mal_text")
+			textArea.clear()
+			for (i,line) in enumerate(file):
+				if i < 8:
+					continue
+				textArea.send_keys(line)
+			button = driver.find_element_by_xpath("//input[@value='Tag The Sentence']")
+			button.click()
+			page_source = driver.page_source
+			soup = BeautifulSoup(page_source, 'html.parser') 
+			for line in soup.find_all(attrs ={"size":"3"}):
+				print(line.string)
+				s+=line.string+'\n'
+		f.write(s.encode("UTF-8"))
 	break
-
-elem.send_keys(Keys.RETURN)
-assert "No results found." not in driver.page_source
